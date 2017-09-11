290ab0e14a3e00441b44e05d2cecda33864f46a3
Kristina Vragovic <k.vragovic@gmail.com>
2017-09-11 14:03:11 -0500

    Create custom routes for root and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  root to: 'pages#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   resources :users
   resources :plants
 
