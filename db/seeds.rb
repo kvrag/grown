d76c59d2d07269c808a5976b582770763657b6ef
Kristina Vragovic <k.vragovic@gmail.com>
2017-09-19 15:23:45 -0500

    Save plant data as csv and parse in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
+require 'csv'
+require_relative 'veg_data'
+
+regularly = Watering.create(frequency: "Regularly")
+weekly = Watering.create(frequency: "Weekly")
+harvest = Watering.create(frequency: "Close to harvest")
+drought = Watering.create(frequency: "Only in drought")
 
 
-regularly = Watering.new(frequency: "Regularly")
-weekly = Watering.new(frequency: "Weekly")
-harvest = Watering.new(frequency: "Close to harvest")
-drought = Watering.new(frequency: "Only in drought")
+CSV.foreach('veg_data.csv', headers:true, header_converters: :symbol) do |row|
+  Plant.create(row.to_h)
+end
